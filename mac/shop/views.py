0cd30818d46d10374ecaecde4ecbc353dbc3258c
coderhouse-ds <sureshangry3@gmail.com>
2021-07-11 10:39:50 +0530

    'tut29completed'

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 
 def index(request):
-    return render(request,'shop/index.html')
+    all_products = Product.objects.all()
+    return render(request,'shop/index.html',{'Products':all_products})
 
 def about(request):
     return HttpResponse("We are at about")
