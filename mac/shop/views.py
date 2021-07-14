e71974ed3d2cb8e43ff906258a80ca289daa84f1
coderhouse-ds <sureshangry3@gmail.com>
2021-07-14 11:10:01 +0530

    'tut41completed'

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,37 +1,47 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Product
 from math import ceil
+
 # Create your views here.
+from django.http import HttpResponse
 
 def index(request):
-    # allProds = [[products,range(1,nSlides),nSlides],[products,range(1,nSlides),nSlides]]
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
     allProds = []
-    catprods = Product.objects.values('category','id')
+    catprods = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catprods}
     for cat in cats:
         prod = Product.objects.filter(category=cat)
         n = len(prod)
         nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        allProds.append([prod,range(1,nSlides),nSlides])
-    params = {'allProds':allProds}
+        allProds.append([prod, range(1, nSlides), nSlides])
 
-    return render(request,'shop/index.html',params)
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
-    return render(request,'shop/about.html')
+    return render(request, 'shop/about.html')
 
 def contact(request):
-    return render(request,'shop/contact.html')
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    return render(request,'shop/tracker.html')
+    return render(request, 'shop/tracker.html')
 
 def search(request):
-    return render(request,'shop/search.html')
+    return render(request, 'shop/search.html')
 
-def productView(request):
-    return render(request,'shop/prodview.html')
+def productView(request, myid):
+    # Fetch the product using the id
+    product = Product.objects.filter(id=myid)
+    return render(request, 'shop/prodView.html', {'product':product[0]})
 
 def checkout(request):
-    return render(request,'shop/checkout.html')
+    return render(request, 'shop/checkout.html')
