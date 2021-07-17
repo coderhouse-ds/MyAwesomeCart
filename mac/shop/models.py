1b5294a44167779a85d93ebce7609c3b846c562a
coderhouse-ds <sureshangry3@gmail.com>
2021-07-17 12:13:32 +0530

    'tut51dompleted'

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -28,7 +28,7 @@ class Contact(models.Model):
 
 class Orders(models.Model):
     order_id = models.AutoField(primary_key=True)
-    items_json = models.CharField(max_length=5000,default="")
+    items_json = models.CharField(max_length=5000)
     name = models.CharField(max_length=90)
     email = models.CharField(max_length=111)
     address = models.CharField(max_length=111)
@@ -36,3 +36,12 @@ class Orders(models.Model):
     state = models.CharField(max_length=111)
     zip_code = models.CharField(max_length=111)
     phone = models.CharField(max_length=111, default="")
+
+class OrderUpdate(models.Model):
+    update_id  = models.AutoField(primary_key=True)
+    order_id = models.IntegerField(default="")
+    update_desc = models.CharField(max_length=5000)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.update_desc[0:7] + "..."
